37450dfc4965d3d95531aa345a35cafa746bd01d
surreal8 <tinashah8@gmail.com>
2017-03-17 11:04:19 -0500

    adding svg as a media upload option

--- stockholm-child/functions.php
+++ stockholm-child/functions.php
@@ -907,3 +907,9 @@ if(!function_exists('qode_excerpt')) {
 		}
 	}
 }
+
+function my_myme_types($mime_types){
+    $mime_types['svg'] = 'image/svg+xml'; //Adding svg extension
+    return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);
